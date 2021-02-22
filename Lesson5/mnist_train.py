dc19f0f0630bd80f0826852b942a0f6b5ad48b99
19831 <19831@DESKTOP-TIBJ9AQ>
2021-02-22 14:07:22 +0800

    add mnist_train,ae file

---
+++ Lesson5/mnist_train.py
@@ -0,0 +1,103 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+
+import torchvision
+import matplotlib.pyplot as plt
+
+from utils_ import plot_image, plot_curve, one_hot
+
+
+batch_size = 512
+
+# step1. load dataset
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data/', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (.1207,), (.3081,))
+                               ])),
+    batch_size=batch_size,shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data/', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (.1307,), (.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=False)
+x, y = next(iter(train_loader))
+print(x.shape, y.shape, x.min(), x.max())
+plot_image(x, y, 'image sample')
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # xw+b
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 10)
+    def forward(self, x):
+        # x: [b, 1, 28, 28]
+        # h1 = relu(xw1+b1)
+        x = F.relu(self.fc1(x))
+        # h2 = relu(h1w2+b2)
+        h2 = F.relu(self.fc2(x))
+        # h3 = h2w3+b3
+        x = self.fc3(h2)
+
+        return x
+
+net = Net()
+
+# [w1, b1, w2, b2, w3, b3]
+optimizer = optim.SGD(net.parameters(), lr=.01, momentum=.9)
+
+train_loss = []
+
+for epoch in range(3):
+    for batch_idx, (x, y) in enumerate(train_loader):
+
+        # x: [b, 1, 28, 28] => y[512]
+        # => [b, 1, 28, 28] => [b, 784]
+        x = x.view(x.size(0), 784)
+        # => [b, 10]
+        out = net(x)
+        # [b, 10]
+        y_onehot = one_hot(y)
+        loss = F.mse_loss(out, y_onehot)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # w' = w - lr*grad
+        optimizer.step()
+
+        train_loss.append(loss.item())
+
+        if batch_idx % 10 == 0:
+            print(epoch, batch_idx, loss.item())
+
+plot_curve(train_loss)
+
+
+total_correct = 0
+for x,y in test_loader:
+    x = x.view(x.size(0), 784)
+    out = net(x)
+    # out: [b, 10] => pred: [b]
+    pred = out.argmax(dim=1)
+    correct = pred.eq(y).sum().float().item()
+    total_correct +=correct
+
+total_num = len(test_loader.dataset)
+acc = total_correct / total_num
+print(f'test acc {acc}')
+
+x, y = next(iter(test_loader))
+out = net(x.view(x.size(0), 784))
+pred = out.argmax(dim=1)
+plot_image(x, pred, 'test')
---
+++ Lesson55/main.py
@@ -0,0 +1,60 @@
+import torch
+from torch.utils.data import DataLoader
+from torch import nn,optim
+from torchvision import transforms, datasets
+
+from ae import AE
+
+import visdom
+
+def main():
+
+    mnist_train = DataLoader(datasets.MNIST('../Lesson5/mnist_data', True, transform=transforms.Compose([
+                    transforms.ToTensor()]),download=True),
+                    batch_size=32, shuffle=True)
+
+
+    mnist_test = DataLoader(datasets.MNIST('../Lesson5/mnist_data', False, transforms.Compose([
+                transforms.ToTensor()]),download=True)
+                , batch_size=32,shuffle=True)
+
+    x, _ = iter(mnist_train).next()
+    print(f'x:{x.shape}')
+
+
+    model = AE()
+    criteon = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    print(model)
+
+    viz = visdom.Visdom()
+
+    for epoch in range(1000):
+
+        for batchidx, (x, _) in enumerate(mnist_train):
+            # [b, 1, 28, 28]
+            x = x
+
+            x_hat,_ = model(x)
+            loss = criteon(x_hat, x)
+
+
+            # backprop
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(epoch, 'loss:', loss.item())
+
+        x, _ = iter(mnist_test).next()
+        x = x
+        with torch.no_grad():
+            x_hat, kld = model(x)
+        viz.images(x, nrow=8, win='x', opts=dict(title='x'))
+        viz.images(x_hat, nrow=8, win='x_hat', opts=dict(title='x_hat'))
+
+
+
+
+if __name__ == '__main__':
+    main()
